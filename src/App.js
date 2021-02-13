5b60e1f941e46be58cf234c53610747e62cf683c
Satish Patel <ssj.satish@gmail.com>
2021-02-13 08:40:50 +0530

    added some styling

--- src/App.js
+++ src/App.js
@@ -30,11 +30,20 @@ class App extends Component {
       ]
     })
   }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      
+      cursor: 'pointer'
+    }
   return (
     <div className="App">
       <h1>React Learning in Progress</h1>
-      <button onClick={this.switchNameHandler}>Switch Name </button>
+      <button style={style} onClick={this.switchNameHandler}>Switch Name </button>
       <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Person.css'
 const person = (props) => {
     return (
-        <div>
+        <div className='Person'>
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} old.</p>
             <p>{props.children}</p>
             
