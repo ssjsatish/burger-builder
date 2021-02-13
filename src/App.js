cbdbda15ee015af44ec22a1b700a0a07cee8c5fe
Satish Patel <ssj.satish@gmail.com>
2021-02-13 07:19:50 +0530

    stateful component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Satish', age: 29 },
       { name: 'Anjali', age: 29 },
       { name: 'Tony Stark', age: 30 }
     ],
     otherState: 'Something'
-  })
-  const switchNameHandler = () => {
-    setPersonsState({
+  }
+
+  switchNameHandler = () => {
+    this.setState({
       persons: [
         { name: 'Satish Patel', age: 29 },
         { name: 'Elon Musk', age: 50 },
@@ -20,27 +21,28 @@ const App = props => {
       ]
     })
   };
-  
+  render() {
   return (
     <div className="App">
       <h1>React Learning in Progress</h1>
-      <button onClick={switchNameHandler}>Switch Name </button>
+      <button onClick={this.switchNameHandler}>Switch Name </button>
       <Person 
-        name={personsState.persons[0].name} 
-        age={personsState.persons[0].age}
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}
       />
       <Person 
-        name={personsState.persons[1].name} 
-        age={personsState.persons[1].age}
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
       />
       <Person 
-        name={personsState.persons[2].name} 
-        age={personsState.persons[2].age}
-        click={switchNameHandler}
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}
+        click={this.switchNameHandler}
       />
     </div>
   );
   }
+}
 
 export default App;
 
