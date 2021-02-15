9345c9bf407e9b4bde4e8e25b7c5ccbe14c72f45
Satish Patel <ssj.satish@gmail.com>
2021-02-15 13:19:48 +0530

    assignment 01

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
+import UserInput from './UserInput/UserInput';
+// import Person from './Person/Person';
+import UserOutput from './UserOutput/UserOutput'
 
 class App extends Component {
   state = {
@@ -32,32 +34,33 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      
-      cursor: 'pointer'
-    }
+    // const style = {
+    //   backgroundColor: 'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+
+    //   cursor: 'pointer'
+    // }
   return (
     <div className="App">
       <h1>React Learning in Progress</h1>
-      <button style={style} onClick={this.switchNameHandler}>Switch Name </button>
-      <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age}
-      />
-      <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        // click={this.switchNameHandler}
-        changed={this.nameChangeHandler} >Hobbies: Reading
-      </Person>
-      <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age}
-      />
+      <ol>
+        <li>Create TWO new Components: UserInput and UserOutput</li>
+        <li>UserInput should hold an input element, UserOutput two paragraphs</li>
+        <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
+        <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
+        <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
+        <li>Add a method to manipulate the state (=> an event-handler method)</li>
+        <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
+        <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
+        <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
+        <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
+      </ol>
+      <UserInput />
+      <UserOutput userName="Satish"/>
+      <UserOutput userName="Max"/>
+      <UserOutput userName="Elon Musk"/>
     </div>
   );
   }
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const userOutput = (props) => {
+    return (
+        <div>
+            <p>UserName: {props.userName}</p>
+            <p>Another random text</p>
+        </div>
+    );
+};
+export default userOutput;
